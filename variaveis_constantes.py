9d0e07fbe5e246ede3d991f963346d0b083af381
Marcelo Mont-Morency Gondim Rodrigues <morencymarcelo@gmail.com>
2024-04-24 22:56:02 -0300

    adicionando a aula variaveis e constantes em python

---
+++ variaveis_constantes.py
@@ -0,0 +1,16 @@
+nome = "marcelo"
+
+idade = 20
+
+nome, idade = ("giovana", 27)
+
+print(nome,idade)
+
+
+limite_saque_diario = 1000
+
+Brazilian_States  = ["SP","RJ","SC","CE"]
+
+Brazilian_States = 10
+
+print(Brazilian_States)
