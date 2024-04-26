5af2fc844d60d8f70d7d3857b37544bd447b7018
Marcelo Mont-Morency Gondim Rodrigues <morencymarcelo@gmail.com>
2024-04-26 17:45:07 -0300

    add arquivo ao github

---
+++ input.py
@@ -0,0 +1,16 @@
+nome = input("Informe  o seu nome: ")
+
+
+nome = "marcelo"
+sobrenome = "morency"
+
+
+print(nome,sobrenome)
+
+print(nome,sobrenome, end="...\n" )
+
+print(nome,sobrenome, sep="#")
+
+
+
+
